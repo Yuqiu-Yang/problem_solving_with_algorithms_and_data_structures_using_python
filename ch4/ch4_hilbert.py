58512d7949ee2e1e852073870ecf68b04b31b8df
Yuqiu Yang <yyqengels@gmail.com>
2020-04-11 19:06:01 -0500

    Create ch4_hilbert.py

---
+++ ch4/ch4_hilbert.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+
+def hilbert(edgeLen, degree, myTurtle, oriantation =1):
+    if degree == 0:
+        return None
+    else:
+        myTurtle.right(90 * oriantation)
+        hilbert(edgeLen, degree - 1, myTurtle, -1 * oriantation)
+        myTurtle.forward(edgeLen)
+        myTurtle.left(90 * oriantation)
+        hilbert(edgeLen, degree - 1, myTurtle, oriantation)
+        myTurtle.forward(edgeLen)
+        hilbert(edgeLen, degree - 1, myTurtle, oriantation)
+        myTurtle.left(90 * oriantation)
+        myTurtle.forward(edgeLen)
+        hilbert(edgeLen, degree - 1, myTurtle, -1 * oriantation)
+        myTurtle.right(90 * oriantation)
+
+myTurtle = Turtle()
+myTurtle.speed("fastest")
+myWin = myTurtle.getscreen()
+myTurtle.up()
+myTurtle.backward(300)
+myTurtle.left(90)
+myTurtle.forward(300)
+myTurtle.right(90)
+myTurtle.down()
+hilbert(5, 7, myTurtle)
+
+myWin.exitonclick()
