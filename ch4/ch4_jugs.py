5225dcbde3d0a58d0421d47ca21cb58be00c1afa
Yuqiu Yang <yyqengels@gmail.com>
2020-04-15 13:23:16 -0500

    Update ch4_jugs.py

--- ch4/ch4_jugs.py
+++ ch4/ch4_jugs.py
@@ -29,9 +29,40 @@ class jug:
 
 
 
-def jugsBrutal(L, S, R):
-    pass
 
+def jugsBrutal(L_maxVolume,S_maxVolume,L_volume, S_volume, R, volume_list):
+
+    if (L_volume + S_volume == R):
+        volume_list[L_volume][S_volume] = True
+        print(f"The volume in large jug is {L_volume}\n\
+        The volume in small jug is {S_volume}")
+        return True
+    else:
+        if volume_list[L_volume][S_volume] == False:
+            volume_list[L_volume][S_volume] = True
+
+            found = jugsBrutal(L_maxVolume,S_maxVolume,0, S_volume, R, volume_list) or \
+                    jugsBrutal(L_maxVolume,S_maxVolume,L_volume, 0, R, volume_list) or \
+                    jugsBrutal(L_maxVolume,S_maxVolume,L_maxVolume, S_volume, R, volume_list) or \
+                    jugsBrutal(L_maxVolume,S_maxVolume,L_volume, S_maxVolume, R, volume_list) or \
+                    jugsBrutal(L_maxVolume,S_maxVolume,max([0,L_volume - (S_maxVolume - S_volume)]), \
+                                min([S_maxVolume, S_volume + L_volume]), R, volume_list) or \
+                    jugsBrutal(L_maxVolume,S_maxVolume,min([L_maxVolume, S_volume + L_volume]),\
+                                max([0, S_volume - (L_maxVolume - L_volume)]), R, volume_list)
+            if found:
+                print(f"The volume in large jug is {L_volume}\n\
+                The volume in small jug is {S_volume}")
+            return found
+        else:
+            return False
+
+L_maxVolume = 5
+S_maxVolume = 3
+volume_list = []
+for i in range(L_maxVolume + 1):
+    temp = [False] * (S_maxVolume + 1)
+    volume_list.append(temp)
+jugsBrutal(L_maxVolume, S_maxVolume, 0,0,4,volume_list)
 
 
 
@@ -90,9 +121,5 @@ def jugProcedure(startJug, endJug, x, y):
     print(f"The volume in {startJug.label} is {startJug.getVolume()}\n \
             The volume in {endJug.label} is {endJug.getVolume()}")
 
-#gcd, x, y = euclidean(5,3)
 
-#Ljug = jug(5, "L")
-#Sjug = jug(3, "S")
-#jugProcedure()
-#jugsEuclidean(21,9,3)
+jugsEuclidean(21,9,3)
