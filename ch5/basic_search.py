c9103db4272c01f95c960c1b90722a8bfbd85bf5
Yuqiu Yang <yyqengels@gmail.com>
2020-04-18 19:48:00 -0500

    Update basic_search.py

--- ch5/basic_search.py
+++ ch5/basic_search.py
@@ -7,3 +7,20 @@ def sequentialSearch(aList, item):
         else:
             pos += 1
     return found
+
+def orderedSequentialSearch(aList, item, order = 'ascending'):
+    pos = 0
+    found = False
+    stop = False
+    while (pos < len(aList)) and (not found) and (not stop):
+        if aList[pos] == item:
+            found = True
+        else:
+            if order == "ascending":
+                if aList[pos] > item:
+                    stop = True
+            else:
+                if aList[pos] < item:
+                    stop = True
+            pos += 1
+    return found
