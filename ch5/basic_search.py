d4660d063f560e9eed1c7d8a1eaf346213ee4a98
Yuqiu Yang <yyqengels@gmail.com>
2020-04-19 13:09:36 -0500

    Updated basic search methods

--- ch5/basic_search.py
+++ ch5/basic_search.py
@@ -24,3 +24,67 @@ def orderedSequentialSearch(aList, item, order = 'ascending'):
                     stop = True
             pos += 1
     return found
+
+
+def binarySearch(aList, item, order = "ascending"):
+    first = 0
+    last = len(aList) - 1
+    found = False
+    while (first <= last) and (not found):
+        midPoint = (first + last)//2
+        if aList[midPoint] == item:
+            found = True
+        else:
+            if order == "ascending":
+                if aList[midPoint] > item:
+                    last = midPoint - 1
+                else:
+                    first = midPoint + 1
+            else:
+                if aList[midPoint] < item:
+                    last = midPoint - 1
+                else:
+                    first = midPoint + 1
+    return found
+
+
+
+def binarySearchRecursive(aList, item, order = "ascending"):
+    if len(aList) == 0:
+        return False
+    else:
+        midPoint = len(aList)//2
+        if aList[midPoint] == item:
+            return True
+        else:
+            if order == "ascending":
+                if aList[midPoint] > item:
+                    return binarySearchRecursive(aList[:midPoint], item, order = "ascending")
+                else:
+                    return binarySearchRecursive(aList[(midPoint + 1):], item, order = "ascending")
+            else:
+                if aList[midPoint] < item:
+                    return binarySearchRecursive(aList[:midPoint], item, order = "descending")
+                else:
+                    return binarySearchRecursive(aList[(midPoint + 1):], item, order = "descending")
+
+
+
+def binarySearchRecursive1(aList, item, first, last, order = "ascending"):
+    if (last < first):
+        return False 
+    else:
+        midPoint = (first + last)//2
+        if aList[midPoint] == item:
+            return True
+        else:
+            if order == "ascending":
+                if aList[midPoint] > item:
+                    return binarySearchRecursive1(aList, item, first, midPoint - 1, order = "ascending")
+                else:
+                    return binarySearchRecursive1(aList, item, midPoint + 1, last, order = "ascending")
+            else:
+                if aList[midPoint] < item:
+                    return binarySearchRecursive1(aList, item, first, midPoint - 1, order = "descending")
+                else:
+                    return binarySearchRecursive1(aList, item, midPoint + 1, last, order = "descending")
