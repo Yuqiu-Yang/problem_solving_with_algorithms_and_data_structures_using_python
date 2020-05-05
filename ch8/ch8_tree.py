d6bba568fada3e8f35c0fd28a1389918c5b05950
Yuqiu Yang <yyqengels@gmail.com>
2020-05-04 21:03:26 -0500

    simple quantization method

---
+++ ch8/ch8_tree.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from PIL import Image
+
+def simpleQuant(imageFile):
+    im = Image.open(imageFile)
+    w, h = im.size
+    for row in range(h):
+        for col in range(w):
+            r, g, b = im.getpixel((col, row))
+            r = r//36*36
+            g = g//42*42
+            b = b//42*42
+            im.putpixel((col, row), (r,g,b))
+    im.show()
