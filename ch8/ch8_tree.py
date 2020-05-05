7e354e2d972f27b7c184486ea6e0d4585e348d37
Yuqiu Yang <yyqengels@gmail.com>
2020-05-05 18:57:49 -0500

    Bugs fixed

--- ch8/ch8_tree.py
+++ ch8/ch8_tree.py
@@ -22,6 +22,7 @@ def buildAndDisplay(imageFile):
     for row in range(h):
         for col in range(w):
             r, g, b = im.getpixel((col, row))
+            #print(f'Inserting {r}_{g}_{b}')
             ot.insert(r, g, b)
     ot.reduce(256)
 
@@ -32,6 +33,11 @@ def buildAndDisplay(imageFile):
             im.putpixel((col, row), (nr, ng, nb))
     im.show()
 
+def buildAndDisplay1(imageFile, maxCubes = 256, method = "individual"):
+    ot = OctTree()
+    ot.openImage(imageFile)
+    ot.buildTree()
+    ot.quantize(maxCubes, method )
 
 class OctTree:
     def __init__(self):
@@ -40,6 +46,27 @@ class OctTree:
         self.numLeaves = 0
         self.leafList = []
         self.leafQueue = PriorityQueue()
+        self.image = None
+    def openImage(self, imageFile):
+        self.image = Image.open(imageFile)
+        self.image.show()
+    def writeImage(self, imageFile):
+        self.image.save(imageFile)
+    def buildTree(self):
+        w, h = self.image.size
+        for row in range(h):
+            for col in range(w):
+                r, g, b = self.image.getpixel((col, row))
+                self.insert(r, g, b)
+    def quantize(self, maxCubes, method = "total"):
+        self.reduce(maxCubes = maxCubes, method = method)
+        w, h = self.image.size
+        for row in range(h):
+            for col in range(w):
+                r, g, b = self.image.getpixel((col, row))
+                nr, ng, nb = self.find(r, g, b)
+                self.image.putpixel((col, row), (nr, ng, nb))
+        self.image.show()
     def insert(self, r, g, b):
         if not self.root:
             self.root = self.otNode(outer = self)
@@ -47,28 +74,47 @@ class OctTree:
     def find(self, r, g, b):
         if self.root:
             return self.root.find(r, g, b, 0)
-    def reduce(self, maxCubes):
+    def reduce(self, maxCubes, method = "total"):
         while len(self.leafList) > maxCubes:
-            smallest = self.findMinCube()
+            smallest = self.findMinCube(method)
             smallest.parent.merge()
             self.leafList.append(smallest.parent)
             self.leafQueue.add((smallest.parent.count, smallest.parent))
             self.numLeaves += 1
-    def findMinCube(self):
-        minCount = sys.maxsize
-        maxLev = 0
-        minCube = None
-        i = self.leafQueue.delMin()
-        minCube = i
-        minCount = i.count
-        maxLev = i.level
-        self.leafQueue.add((i.count, i))
-        #for i in self.leafList:
-        #    if i.count <= minCount and i.level >= maxLev:
-        #        minCube = i
-        #        minCount = i.count
-        #        maxLev = i.level
-        return minCube
+    def findMinCube(self, method = "total"):
+        if method != "individual":
+            minCount = sys.maxsize
+            maxLev = 0
+            minCube = None
+            i = self.leafQueue.delMin()
+            minCube = i
+            minCount = i.count
+            maxLev = i.level
+            self.leafQueue.add((i.count, i))
+            #for i in self.leafList:
+            #    if i.count <= minCount and i.level >= maxLev:
+            #        minCube = i
+            #        minCount = i.count
+            #        maxLev = i.level
+            return minCube
+        else:
+            tempDic = {}
+            for i in self.leafList:
+                if not i.parent in tempDic:
+                    tempDic[i.parent] = i.count
+                else:
+                    tempDic[i.parent] += i.count
+            minCount = sys.maxsize
+            minKey = None
+            for c in tempDic.keys():
+                if tempDic[c] < minCount:
+                    minCount = tempDic[c]
+                    minKey = c
+            for i in range(8):
+                if minKey.children[i]:
+                    return minKey.children[i]
+
+
     ####
     # Inner class otNode
     ####
