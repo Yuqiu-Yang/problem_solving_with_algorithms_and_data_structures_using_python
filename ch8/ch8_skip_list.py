0e772a9ba8ff28a6d70c5933408aca57dd93db0e
Yuqiu Yang <yyqengels@gmail.com>
2020-05-04 20:30:50 -0500

    Skip list deletion method

--- ch8/ch8_skip_list.py
+++ ch8/ch8_skip_list.py
@@ -67,7 +67,7 @@ class SkipList:
         else:
             return None
     def insert(self, key, data):
-        if self.head = None:
+        if self.head == None:
             self.head = HeaderNode()
             temp = DataNode(key, data)
             self.head.setNext(temp)
@@ -117,8 +117,45 @@ class SkipList:
                     temp.setDown(top)
                     temp.setNext(nextLevel.getNext())
                     nextLevel.setNext(temp)
-                    top = temp 
+                    top = temp
+    def __delitem__(self, key):
+        current = self.head
+        found = False
+        stop = False
+        towerStack = Stack()
+        while not found and not stop:
+            if current == None:
+                stop = True
+            else:
+                if current.getNext() == None:
+                    current = current.getDown()
+                else:
+                    if current.getNext().getKey() == key:
+                        found = True
+                    else:
+                        if key < current.getNext().getKey():
+                            current = current.getDown()
+                        else:
+                            current = current.getNext()
 
+        if found:
+            stop = False
+            while not stop:
+                if current == None:
+                    stop = True
+                else:
+                    if current.getNext().getKey() == key:
+                        towerStack.push(current)
+                        current = current.getDown()
+                    else:
+                        current = current.getNext()
+            while not towerStack.isEmpty():
+                nodeBefore = towerStack.pop()
+                nodeBefore.getNext().setDown(None)
+                temp = nodeBefore.getNext().getNext()
+                nodeBefore.setNext(temp)
+        else:
+            raise KeyError('Key is not in the list')
 
     def __getitem__(self, key):
         return self.search(key)
@@ -129,11 +166,88 @@ class SkipList:
             return True
         else:
             return False
+    def keys(self):
+        current = self.head
+        stop = False
+        while not stop:
+            if current.getDown() == None:
+                stop = True
+            else:
+                current = current.getDown()
+        keyList = []
 
+        stop = False
+        while not stop:
+            if current.getNext() == None:
+                stop = True
+            else:
+                current = current.getNext()
+                keyList.append(current.getKey())
+        return keyList
+    def values(self):
+        current = self.head
+        stop = False
+        while not stop:
+            if current.getDown() == None:
+                stop = True
+            else:
+                current = current.getDown()
+        valueList = []
+
+        stop = False
+        while not stop:
+            if current.getNext() == None:
+                stop = True
+            else:
+                current = current.getNext()
+                valueList.append(current.getData())
+        return valueList
+    def printWholeList(self):
+        keyList = self.keys()
+        listRep = []
+        towerStack = Stack()
+        current = self.head
+        stop = False
+        while not stop:
+            towerStack.push(current)
+            if current.getDown() == None:
+                stop = True
+            else:
+                current = current.getDown()
+        while not towerStack.isEmpty():
+            currentHead = towerStack.pop()
+            temp = []
+            stop = False
+            current = currentHead.getNext()
+            for c in range(len(keyList)):
+                if current == None:
+                    temp.append((None, None)) 
+                else:
+                    if current.getKey() != keyList[c]:
+                        temp.append((None, None))
+                    else:
+                        temp.append((current.getKey(), current.getData()))
+                        current = current.getNext()
+
+
+            listRep.append(temp)
+        return listRep[::-1]
 class Map:
     def __init__(self):
         self.collection = SkipList()
     def put(self, key, data):
         self.collection.insert(key, data)
     def get(self, key):
-        self.collection.search(key)
+        return self.collection.search(key)
+    def __getitem__(self, key):
+        return self.get(key)
+    def __setitem__(self, key, data):
+        self.put(key, data)
+    def keys(self):
+        return self.collection.keys()
+    def values(self):
+        return self.collection.values()
+    def __contains__(self, key):
+        return key in self.collection
+    def __delitem__(self, key):
+        del self.collection[key]
